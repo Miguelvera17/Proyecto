b8b48dfe65cc39e43255535a02e35392404ff335
Miguel Vera <2023_miguel.vera@iticbcn.cat>
2024-10-20 21:37:56 +0200

    in progress

--- API/main.py
+++ API/main.py
@@ -1,9 +1,19 @@
 import mysql.connector
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 def get_db_connection():
     connection = mysql.connector.connect(
